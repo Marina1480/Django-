3de0fc08cc9c28da31aba55daf21c1b9a9b404c6
Marina1480 <85301606+Marina1480@users.noreply.github.com>
2022-06-13 22:47:15 +0300

    Update admin.py

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 
-from .models import Article
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, ArticleTag
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title']
+
+class ArticleTagInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        tags_ids = set([form.clened_data['tag'].id for form in self.forms])
+        if len(tags_ids) != len(self.forms):
+            raise ValidationError('Ошибка!')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleTagInline(admin.TabularInline):
+    model = ArticleTag
+    formset = ArticleTagInlineFormset
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [ArticleTagInline,]
