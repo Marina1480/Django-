81188169198f689345fbf422e9fcab1dabb80c36
Marina1480 <85301606+Marina1480@users.noreply.github.com>
2022-06-13 22:42:44 +0300

    Update admin.py

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -5,9 +5,11 @@ from .models import Student, Teacher
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name']
+
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'subject']
+    list_filter = ['subject']
